ff72c824373d3545744aaa2b4bc8380647ea3e3d
Ben Cornelis <bencornelis02@gmail.com>
2018-07-28 21:12:17 -0500

    removes docker, rewrites travis.yml to use deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,2 @@
-cd /app
-npm run build
-
 cd build
-aws s3 sync ./ s3://$S3_BUCKET --acl public-read --cache-control "max-age=86400"
+aws s3 sync ./ s3://conway-game-of-life --acl public-read --cache-control "max-age=86400"
