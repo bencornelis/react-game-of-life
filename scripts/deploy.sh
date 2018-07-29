64c3558a1842a01c2ef5b79ffe9962f270bfbe67
Ben Cornelis <bencornelis02@gmail.com>
2018-07-29 10:10:21 -0500

    moves cloudfront cache invalidation into deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 cd build
 aws s3 sync ./ s3://$S3_BUCKET --acl public-read --cache-control "max-age=86400"
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/index.html"
